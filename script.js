73365ea7760cff816461621fd17889ebb2b46192
Valentin Caraba <luciancaraba@gmail.com>
2020-08-10 02:26:52 +0200

    Created function to add the grid.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 let container = document.querySelector('#container');
-let counter = 1;
+let rows; 
+let columns;
 
-for
+function createGrid(nr1, nr2) {
+    createRows(nr1);
+    createCols(nr2);
+}
+
+function createRows(rowsNumber) {
+    for(let i = 0; i < rowsNumber; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('row');
+        container.appendChild(cell);
+    }
+    rows = document.querySelectorAll('.row');
+}
+
+function createCols(colsNumber) {
+    for(let i = 0; i < rows.length; i++) {
+        for(let j = 0; j < colsNumber; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('column');
+            rows[j].appendChild(cell);
+        }
+    }
+    columns = document.querySelectorAll('.column');
+}
