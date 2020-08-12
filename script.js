2e4a304c2726b5bd0d92f7ceb24132436dedb8b4
Valentin Caraba <luciancaraba@gmail.com>
2020-08-13 00:54:07 +0200

    errors with new grid function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let container = document.querySelector('#container');
 let btnReset = document.querySelector('#reset-btn');
+let btnNewGrid = document.querySelector('#create-btn');
 let rows; 
 let columns;
+let currentRow = 16;
+let currentCols = 16;
 
-createGrid(16, 16);
+createGrid(currentRow, currentCols);
 
 function createGrid(nr1, nr2) {
     createRows(nr1);
@@ -19,6 +22,12 @@ function createRows(rowsNumber) {
     rows = document.querySelectorAll('.row');
 }
 
+function deleteRows(rowsNumber) {
+    for(let i = 0; i < rowsNumber; i++) {
+        container.removeChild(cell);
+    }
+}
+
 function createCols(colsNumber) {
     for(let i = 0; i < rows.length; i++) {
         for(let j = 0; j < colsNumber; j++) {
@@ -29,6 +38,19 @@ function createCols(colsNumber) {
         }
     }
     columns = document.querySelectorAll('.column');
+}
+
+function deleteCols(colsNumber) {
+    for(let i = 0; i < rows.length; i++) {
+        for(let j = 0; j < colsNumber; j++) {
+            rows[i].removeChild(cell);
+        }
+    }
+}
+
+function deleteGrid(nr1, nr2) {
+    deleteCols(nr2);
+    deleteRows(nr1);
 }
 
 columns.forEach(item => {
@@ -43,5 +65,15 @@ function reset() {
     })
 }
 
+function newGrid() {
+    var newLength = prompt("Select 16, 32 or 64");
+    deleteGrid(currentRow, currentCols);
+    currentRow = newLength;
+    currentCols = newLength;
+    createGrid(currentRow, currentCols);
+}
+
 btnReset.addEventListener('click', reset);
+
+btnNewGrid.addEventListener('click', newGrid);
 
